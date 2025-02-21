f9917b1ca5218b1cd5eb66a823b1a9cfd6deb718
Sukeshdaws <sukeshg613@gamil.com>
2025-02-21 15:08:18 +0530

    mysql

--- 01-packages.sh
+++ 01-packages.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAM.log
 R="\e[31m"
 G="\e[32m"
@@ -22,11 +22,20 @@ fi
 
 }
 
-
-
 if [ $USERID -ne 0]
 then
     echo "run this script with root access"
+    exit 1
 
  else
     echo  "you are a super user"
+
+    dnf install mysql-server -y &>>$LOGFILE
+    VALIDATE $? "Installing MYSQL Server"
+
+    systemctl enable mysqld &>>LOGFILE
+    VALIDATE $? "Enabling MYSQL Server"
+
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
+    VALIDATE $? "Setting up root password"
+
