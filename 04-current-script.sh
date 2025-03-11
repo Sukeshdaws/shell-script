675c7689bf5609d5f715a8a32257ec1feb14fe11
Sukeshdaws <sukeshg613@gamil.com>
2025-03-11 10:53:04 +0530

    mysql

--- 04-current-script.sh
+++ 04-current-script.sh
@@ -5,6 +5,6 @@ COURSE="DevOps from current script"
 echo "Before calling the script,course: $COURSE"
 echo "Process ID of current script is : $$"
 
-./05-other-script.sh
+./ 05-other-script.sh
 
 echo "After calling other script,course: $COURSE"
